9d6bd523d6c544ed041585933486655d3e84e906
lwjmzla <214245088@qq.com>
2020-12-12 00:07:38 +0800

    // todo如果webpack-replace-loader或者string-replace-loader 用不了，注意打包的时候需要手动给全局/img 替换为/badminton/img

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
-module.exports = {
-  publicPath: '/badminton', // !设置 process.env.BASE_URL的值
+
+let isProd = process.env.NODE_ENV === 'production';
+let publicPath = '/badminton';
+let configObj = {
+  publicPath: publicPath, // !设置 process.env.BASE_URL的值
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: process.env.NODE_ENV === 'development' ? 'source-map' : 'cheap-source-map'
   },
+  // devServer: { // !关闭eslint
+  //   overlay: {
+  //     warnings: false,
+  //     errors: false
+  //   },
+  //   lintOnSave: false
+  // },
   css: {
     loaderOptions: {
       scss: {
@@ -10,5 +20,35 @@ module.exports = {
         prependData: `@import "~@/styles/variables.scss";` // !当前sass-loader为"version": "8.0.2"，所以用prependData属性，否则用additionalData
       }
     }
+  },
+  chainWebpack: (config) => {
+    // todo如果webpack-replace-loader或者string-replace-loader 用不了，注意打包的时候需要手动给全局/img 替换为/badminton/img
+    // if (isProd) {
+    // config.module
+    //   .rule('replace')
+    //   .use('webpack-replace-loader')
+    //   .loader('webpack-replace-loader')
+    //   .tap(options => {
+    //     options = {
+    //       arr: [
+    //         { search: '/img', replace: `${publicPath}/img`, attr: 'g' }
+    //       ]
+    //     };
+    //     return options;
+    //   });
+    // !在vue-cli  chainWebpack  就各种bug 好奇葩
+    // !https://cli.vuejs.org/zh/guide/webpack.html#替换一个规则里的-loader
+    // !https://www.jianshu.com/p/3529d2d1e2f9/    vue-cli-service inspect  'image-webpack-loader' https://segmentfault.com/q/1010000016724383
+    // config.module
+    //   .rule('replace')
+    //   .use('string-replace-loader')
+    //   .loader('string-replace-loader')
+    //   .options({
+    //     search: '/img',
+    //     replace: `${publicPath}/img`,
+    //     flags: 'g'
+    //   });
   }
+  // }
 };
+module.exports = configObj;
